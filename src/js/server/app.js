0afc087c66c0d79cac7a0ca5da0e5d9b5ff8d91b
Tom Corke <tom.corke@skyscanner.net>
2018-01-30 11:35:57 +0000

    Temp!

--- src/js/server/app.js
+++ src/js/server/app.js
@@ -2,6 +2,7 @@ import path from 'path';
 
 import express from 'express';
 import compress from 'compression';
+import sqlite3 from 'sqlite3';
 
 import log from './logging';
 import { getPrimeDropData } from './prime-drop-data';
@@ -11,6 +12,26 @@ const app = express();
 process.on('uncaughtException', err => log.error(err));
 
 app.use(compress());
+
+app.get('/db/:privatekey', (req, res) => {
+    let key = req.params.privatekey;
+
+    let db = new sqlite3.Database('./warframe.live.sqlite');
+
+    db.serialize(() => {
+
+        let stmt = db.prepare('SELECT publickey, jsondata, timestamp FROM wf_user_data WHERE privatekey = ?');
+        stmt.get(key, (err, result) => {
+            if (err) { throw err; }
+            if (!result) { throw 'No result'; }
+            res.json(result);
+        });
+        stmt.finalize();
+
+    });
+
+    db.close();
+});
 
 app.get('/prime-data', (req, res) => {
 
--- src/js/server/index.js
+++ src/js/server/index.js
@@ -16,3 +16,32 @@ log.info('Starting HTTP server');
 server.listen(port, function() {
     log.debug(`Successfully started server, listening on port ${port}`);
 });
+
+const masteryData = JSON.parse(fs.readFileSync('./mastery_data.json'));
+let newMasteryData = {};
+
+let type = null;
+masteryData.data.forEach(item => {
+    type = item.type || type;
+    let name = item.name;
+    newMasteryData[type] = newMasteryData[type] || {};
+    newMasteryData[type][name] = { id: uuid.v4() };
+});
+fs.writeFileSync('./new_mastery_data.json', JSON.stringify(newMasteryData, null, 4));
+
+const primeData = JSON.parse(fs.readFileSync('./prime_data.json'));
+let newPrimeData = {};
+
+let primeType = null;
+let primeName = null;
+let primePartName = null;
+primeData.data.forEach(item => {
+    primeType = item.primetype || primeType;
+    primeName = item.primename || primeName;
+    primePartName = item.partname || primePartName;
+    let newPrimeType = newPrimeData[primeType] = newPrimeData[primeType] || {};
+    let newPrimeName = newPrimeType[primeName] = newPrimeType[primeName] || { id: uuid.v4(), parts: {}};
+    newPrimeName.parts[primePartName] = newPrimeName.parts[primePartName] || { id: uuid.v4() };
+});
+
+fs.writeFileSync('./new_prime_data.json', JSON.stringify(newPrimeData, null, 4));
